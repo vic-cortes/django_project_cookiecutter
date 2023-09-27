3674704746b85d18ffd849109839fd5b4c61d857
Víctor M. Cortés Figueroa <victor.cortes@unosquare.com>
2023-09-26 19:57:38 -0500

    Added Dockerfile & compose

--- {{ cookiecutter.project_slug }}_project/config/settings/local.py
+++ {{ cookiecutter.project_slug }}_project/config/settings/local.py
@@ -3,3 +3,12 @@ from .base import *
 DEBUG = True
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "{{ cookiecutter.project_slug }}_db",
+        "HOST": "localhost",
+    }
+}
+
+INSTALLED_APPS += []
